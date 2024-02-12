e09f0b356ac563909fe2038e65c8101ad4737259
Hamed Ghazikhani <hamed.ghazikhani@gmail.com>
2024-02-12 13:41:55 -0500

    some adjustments

--- app.py
+++ app.py
@@ -3,25 +3,29 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/submit_sequence', methods=['POST'])
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/submit_sequence", methods=["POST"])
 def submit_sequence():
-    sequence = request.form['proteinSequence']
+    sequence = request.form["proteinSequence"]
     # Process the sequence
     # For now, just return it as a response
-    return jsonify({'sequence': sequence})
+    return jsonify({"sequence": sequence})
 
-@app.route('/submit_file', methods=['POST'])
+
+@app.route("/submit_file", methods=["POST"])
 def submit_file():
-    if 'fastaFile' in request.files:
-        fasta_file = request.files['fastaFile']
+    if "fastaFile" in request.files:
+        fasta_file = request.files["fastaFile"]
         # Process the file
         # For now, just return the file name as a response
-        return jsonify({'file_name': fasta_file.filename})
-    return jsonify({'error': 'No file found'})
+        return jsonify({"file_name": fasta_file.filename})
+    return jsonify({"error": "No file found"})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
